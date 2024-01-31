37e8ae9f9bc0ca03b6ecdbe00cf9fc2bf33068f6
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-01-31 14:58:02 +0530

    all fules

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Home from './components/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import S from './components/S'
+import Enquiry from './components/Enquiry'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Enquiry/>}/>
         <Route path='/succsess' element={<S/>}/>
       </Routes>
       </BrowserRouter>
---
+++ src/components/Enquiry.jsx
@@ -0,0 +1,198 @@
+import React, { useState } from 'react';
+import '../styels/home.css'
+
+const Enquiry = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    gender: '',
+    dob:"",
+    address:"",
+    Gmobile:"",
+    mobile:""
+    
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleNext = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handlePrev = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
+  const handleSubmit = () => {
+    // Implement your registration logic here
+    console.log('Registration Data:', formData);
+  };
+
+  return (
+  <div className="container-fluid enquiry bg-light justify-content-center">
+     <div className="row  p-3  align-items-center bg-white rounded-3 border">
+    <div className="col-12 col-md-6">
+    <img src="https://img.freepik.com/free-vector/sign-up-concept-illustration_114360-7885.jpg?size=626&ext=jpg&ga=GA1.1.874872603.1694171926&semt=sph" alt="" className="w-100" />
+    </div>
+    <div className="col-12 col-md-6">
+        <div className="enquiry-right">
+        <div>
+      {step === 1 && (
+        <div>
+          <h2>Step 1: Basic Information</h2>
+          <label>
+            Username:
+           
+          </label> <br />
+          <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className='form-control'
+              placeholder='Enter Your name'
+            />
+            <div className="d-flex flex-nowrap gap-2 align-items-center">
+            
+           <label htmlFor="" className="form-label w-100">
+            select gender
+           <select 
+             name="gender"
+             value={formData.gender}
+             onChange={handleChange}
+             className='form-control'
+            >
+                <option value="" selected disabled>Gender</option>
+                <option value="male">male</option>
+                <option value="female">female</option>
+
+            </select>
+           </label>
+             <label htmlFor="" className="form-label w-100">
+                Date of birth
+             <input
+              type="date"
+              name="dob"
+              value={formData.dob}
+              onChange={handleChange}
+              className='form-control'
+              placeholder='Enter Your name'
+            />
+             </label>
+            </div>
+            
+        <div className="text-center d-flex gap-3 flex-wrap">
+        <button onClick={handleNext} className='bg-warning btn text-white mt-3 w-100 fs-5'>Next</button>
+        </div>
+        </div>
+      )}
+
+      {step === 2 && (
+     <div>
+     <h2>Hello👋, {formData.username}</h2>
+     <p className="fs-6 text-secondary">May i Know your Address</p>
+
+      <label htmlFor="" className="form-label w-100  mt-3">
+        Address
+      <textarea 
+        name="address"
+        value={formData.address}
+        onChange={handleChange}
+        className='form-control'
+        placeholder='Enter Your Address'
+       cols="30" rows="5"></textarea>
+      </label>
+   <div className="text-center d-flex gap-3 flex-wrap flex-md-nowrap">
+   <button onClick={handlePrev} className='bg-dark btn text-white mt-3 w-100 fs-5'>Previous</button>
+   <button onClick={handleNext} className='bg-warning btn text-white mt-3 w-100 fs-5'>Next</button>
+   </div>
+   </div>
+      )}
+
+      {step === 3 && (
+         <div>
+         <h2>Hai <span className="text-warning">{formData.username}</span></h2>
+         <p className="fs-6 text-secondary">Please fill your Basic Information</p>
+         <label>
+           Email
+          
+         </label> <br />
+         <input
+             type="text"
+             name="email"
+             value={formData.username}
+             onChange={handleChange}
+             className='form-control'
+             placeholder='Enter Your name'
+           />
+                <div className="d-flex flex-nowrap gap-2 align-items-center">
+            
+            <label htmlFor="" className="form-label w-100">
+             Mobile No
+             <input
+               type="tel"
+               name="mobile"
+               value={formData.mobile}
+               onChange={handleChange}
+               className='form-control'
+               placeholder='000-0000-000'
+             />
+            </label>
+              <label htmlFor="" className="form-label w-100">
+                guardian's No
+              <input
+               type="tel"
+               name="Gmobile"
+               value={formData.Gmobile}
+               onChange={handleChange}
+               className='form-control'
+               placeholder='000-0000-000'
+             />
+              </label>
+             </div>
+             <div className="d-flex flex-nowrap gap-2 align-items-center">
+            
+            <label htmlFor="" className="form-label w-100">
+            Qualification
+             <input
+               type="tel"
+               name="qualification"
+               value={formData.qualification}
+               onChange={handleChange}
+               className='form-control'
+               placeholder='Your Degree'
+             />
+            </label>
+              <label htmlFor="" className="form-label w-100">
+                year of passing
+              <input
+               type="date"
+               name="passing"
+               value={formData.passing}
+               onChange={handleChange}
+               className='form-control'
+             />
+              </label>
+             </div>
+       <div className="text-center d-flex gap-3 flex-wrap flex-md-nowrap">
+       <button onClick={handlePrev} className='bg-dark btn text-white mt-3 w-100 fs-5'>Previous</button>
+       <button onClick={handleNext} className='bg-warning btn text-white mt-3 w-100 fs-5'>Next</button>
+       </div>
+       </div>
+      )}
+    </div>
+        </div>
+    </div>
+   </div>
+  </div>
+  );
+};
+
+export default Enquiry;
