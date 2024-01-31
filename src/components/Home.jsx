14fa0fd030074e489a4c9f0ba5360e666f2e79eb
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-01-31 09:58:47 +0530

    all fules

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function Home() {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
+    const [first, setFirst ] = useState(true)
+    const [isFunctionActive, setIsFunctionActive] = useState(true);
+    useEffect(() => {
+        if (isFunctionActive) {
+          const timeoutId = setTimeout(() => {
+            // Your function logic goes here
+            console.log('Function executed after some time');
+            setFirst(false)
+    
+            // Close the function by updating state
+            setIsFunctionActive(false);
+          }, 3000); // Change 2000 to the desired delay in milliseconds
+    
+          // Cleanup function to clear the timeout when the component unmounts or when the function is closed
+          return () => clearTimeout(timeoutId);
+        }
+      }, [isFunctionActive]);
+    
     
   const [formData, setFormData] = useState({
     username: "",
@@ -50,9 +69,25 @@ function Home() {
     }
   };
   return (
-  <div className="conatiner register p-3">
-   
+  <div className="conatiner register p-0 overflow-hidden">
     {
+        first?(
+<>
+<div className="first bg-white">
+   <div className="row">
+    <div className="col-md-6 offset-md-3">
+    <img src="https://i.pinimg.com/originals/47/03/09/4703093a70ba47001bf2c86319aae091.gif" alt=""  className='w-100'/>
+    <h1 className="fs-1">Walking to Be practical</h1>
+    <p className="fs-6 text-center text-secondary">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt impedit aspernatur eveniet officia alias doloribus culpa nihil facilis eaque et, atque rerum consequatur accusantium hic cum, cumque nisi similique at.
+    </p>
+    </div>
+   </div>
+</div>
+</>
+        ):(
+            <div className='p-2'>
+             {
         loading ?(
             <div className="loader">
                 <div class="spinner-border text-warning" role="status">
@@ -162,6 +197,11 @@ function Home() {
             </>
         )
     }
+            </div>
+        )
+    }
+   
+   
   </div>
   )
 }
