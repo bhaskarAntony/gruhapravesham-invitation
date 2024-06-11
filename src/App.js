22d78fee854b8c1759599accd6550441948c0922
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-06-12 00:28:36 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header';
@@ -7,8 +7,12 @@ import EventDetails from './components/EventDetails';
 import Schedule from './components/Schedule';
 import RSVP from './components/RSVP';
 import Footer from './components/Footer';
+import Aos from 'aos';
 
 function App() {
+  useEffect(() => {
+    Aos.init(); // Initialize AOS
+  }, []);
   return (
     <div className="App">
       <Header />
@@ -19,13 +23,16 @@ function App() {
       
       <Schedule />
       {/* <RSVP /> */}
-
-      <div className='map p-5'>
+      
+      <div className='map p-5' data-aos="fade-down">
         <h1 className='display-1 i text-center text-white'>Location</h1>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.2900664994245!2d77.63482660000001!3d12.8890605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae14b9ec869fd1%3A0xe502eb43ff449523!2s1st%20Main%20Rd%2C%20Mico%20Layout%2C%20Hongasandra%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1718129987192!5m2!1sen!2sin" width="600" height="450" className='w-100 rounded-4' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
-      <img src='https://static.vecteezy.com/system/resources/thumbnails/010/985/457/small/gold-volumetric-3d-text-balloons-thank-you-cut-out-png.png' className='thanks'/>
+      <div className='bg'></div>
+      <img src='https://cdn.pixabay.com/photo/2022/07/20/10/16/woman-7333869_1280.png' className='thanks mt-3' data-aos="flip-down"/>
+      <img src='https://static.vecteezy.com/system/resources/thumbnails/010/985/457/small/gold-volumetric-3d-text-balloons-thank-you-cut-out-png.png' className='thanks' data-aos="fade-up"/>
+      <div className='bg'></div>
       <Footer />
     </div>
   );
--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -8,19 +8,19 @@ function Schedule() {
       <div className="container">
         <div className="row">
           <div className="col-md-4 mb-3">
-            <div className="timeline-item h-100">
+            <div className="timeline-item h-100" data-aos="fade-up">
               <span className="time d-block display-5 fw-bold">5:00 am</span>
               <span className="activity fs-4 d-block mt-3 i">Gruhapravesha</span>
             </div>
           </div>
           <div className="col-md-4 mb-3">
-            <div className="timeline-item h-100">
+            <div className="timeline-item h-100" data-aos="fade-up">
               <span className="time d-block display-5 fw-bold">10:30 am</span>
               <span className="activity fs-4 mt-3 d-block i">Sathyanarayana Swamy Pooja</span>
             </div>
           </div>
           <div className="col-md-4 mb-3">
-            <div className="timeline-item h-100">
+            <div className="timeline-item h-100" data-aos="fade-up">
               <span className="time d-block display-5 fw-bold">12:30 pm</span>
               <span className="activity fs-4 mt-3 d-block i">Lunch</span>
             </div>
